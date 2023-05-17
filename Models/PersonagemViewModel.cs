588cc0caa58b1884b5d9ab0cd5115425637629c5
Hugofriedrich369 <hungos0223@gmail.com>
2023-05-16 22:01:59 -0300

    first commit

---
+++ Models/PersonagemViewModel.cs
@@ -0,0 +1,25 @@
+using RpgMvc.Models.Enuns;
+
+namespace RpgMvc.Models
+{
+    public class PersonagemViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        public ClasseEnum Classe { get; set; }
+        public byte[]? FotoPersonagem { get; set; }
+        // public Usuario Usuario { get; set; }
+        // public Arma Arma { get; set; }
+
+        // public List<PersonagemHabilidade> PersonagemHabilidades { get; set; }
+
+        public int Disputas { get; set; }
+        public int Vitorias { get; set; }
+        public int Derrotas { get; set; }
+
+    }
+}
